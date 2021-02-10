3f27161d6fc877ff652499d2fbece41e645c8260
namrata3098 <60501115+namrata3098@users.noreply.github.com>
2021-02-10 10:05:32 +0530

    Styling-Basics

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,24 +11,42 @@ state = {
   ]
 }
 
-switchEventHandler = () => {
+switchEventHandler = ( newName) => {
   this.setState({
       person : [
-        {name : "Maxilian" , age: 28},
+        {name : newName , age: 28},
         {name: "Manu" , age: 29},
         {name: "Alex", age: 23}
       ]
   })
 }
 
+nameChangeHandler = ( event) => {
+  this.setState({
+      person : [
+        {name : "max" , age: 28},
+        {name:  event.target.value , age: 29},
+        {name: "Alex", age: 23}
+      ]
+  })
+}
+
   render() {
+    const style = {
+      border: '1px solid blue',
+      backgroundColor : 'white',
+      padding : '8px',
+      font : 'inherit',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h5>Hi, I am a React App</h5>
         <p> I do wonders </p>
-        <button onClick={this.switchEventHandler}>Switch name</button>
-        <Person name = {this.state.person[0].name} age= { this.state.person[0].age}></Person>
-        <Person name = {this.state.person[1].name} age= { this.state.person[1].age}> Hobbies: Creating Apps</Person>
+        <button style= {style} onClick={ () => this.switchEventHandler("Maximillian!!")}>Switch name</button>
+        <Person  click= { this.switchEventHandler.bind(this,"Max!")} name = {this.state.person[0].name} age= { this.state.person[0].age}></Person>
+        <Person  change= {this.nameChangeHandler} name = {this.state.person[1].name} age= { this.state.person[1].age} > Hobbies: Creating Apps</Person>
         <Person name = {this.state.person[2].name} age= { this.state.person[2].age}></Person>
       </div>
     );
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     return  (
-    <div>
-    <p>Holla There!I am {props.name} , my age is { props.age} </p>
+    <div className="Person">
+    <p onClick = {props.click}>Holla There!I am {props.name} , my age is { props.age} </p>
     <p>{props.children}</p>
+    <input type="text" onChange = {props.change}></input>
     </div>
     )
 }
